09acd9dadf5a64ed615cbd7c252c0d48aba2bc57
adamharrison <adamharrison@users.noreply.github.com>
2021-12-09 00:29:41 +0000

    deploy: ff7296370ef60ade1c0ac48350952c6ea6a296ba

---
+++ en/index.md
@@ -0,0 +1,7 @@
+# Lite XL
+
+A lightweight, *simple*, fast, feature-filled, and extremely extensible text editor written in C, and Lua, adapted from [lite](https://github.com/rxi/lite/).
+
+![](img/editor.png)
+
+
---
+++ scripts/site.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'redcarpet'
+require 'rouge'
+class RedRouge < Redcarpet::Render::HTML
+   def block_code(code, language) "<pre>" + Rouge.highlight(code, language || "bash", 'html') + "</pre>" end
+ end
+rc = Redcarpet::Markdown.new(RedRouge.new(with_toc_data: true), { fenced_code_blocks: true, tables: true, footnotes: true })
+sitemap = File.open("sitemap.txt", "w")
+File.write("index.html", File.read('template.html').gsub("{{ pages }}", "<style type='text/css'>" + Rouge::Themes::Base16.mode(:dark).render(scope: 'pre') + "</style>" +
+  Dir.glob("en/**/*").select { |x| File.file?(x) }.map { |x| 
+    sitemap.write("https://lite-xl.com" + (x =~ /index\.md/ ? "" : "?/" + x.downcase.gsub(/^en\//, "").gsub(/\.\w+$/, "")) + "\n")
+    "<page id='page-#{x.downcase.gsub(/(^en\/[^a-z0-9_\-\/\\]|\.\w+$)/,"").gsub(/[\s\\\/]+/, "-")}'>#{rc.render(File.read(x))}</page>" 
+  }.join("\n"))
+)
